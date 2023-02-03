7b2dc77bea84f4206091574172598ad8b29d78c5
ale4287 <99696139+ale4287@users.noreply.github.com>
2023-02-03 18:22:03 -0300

    Commit Inicial

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+Envío de formulario de contacto por correo 
+con el API de Formspree
+*/
+
+const $form = document.querySelector('#form')
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  const response = await fetch(this.action, {
+    method: this.method,
+    body: form,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }) 
+  if (response.ok){
+    this.reset()
+    alert('Gracias por contactarse, me comunicare pronto!')
+  }
+}
